234aa70bb1d8f92b6f7a0b877255ac61e753c3d7
FranderCT <09.reelaborar-cerosa@icloud.com>
2025-10-31 21:33:46 -0600

    cambios

--- src/api/altosdelvalle.ts
+++ src/api/altosdelvalle.ts
@@ -3,7 +3,32 @@ import axios from "axios";
 const altosDelValleAPI = axios.create({
     baseURL: 'http://localhost:3000',
     withCredentials: true,
-
+    // header de autorizacion si es necesario
+    headers: {
+        'Content-Type': 'application/json',
+    },
 })
+
+altosDelValleAPI.interceptors.request.use(
+  (config: any) => {
+    config.headers = config.headers ?? {};
+
+    const hasAuthHeader =
+      !!config.headers["Authorization"] || !!config.headers["authorization"];
+
+    if (hasAuthHeader) {
+      return config; 
+    }
+
+    const sessionToken = localStorage.getItem("token");
+    if (sessionToken && sessionToken !== "null" && sessionToken !== "undefined") {
+      config.headers["Authorization"] = `Bearer ${sessionToken}`;
+    } else {
+      delete config.headers["Authorization"];
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default altosDelValleAPI
